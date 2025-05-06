bc6c5be98ea813bc83828c2e5c82bf53941a672f
muhammad al ghifari <muhamadalghifari97@gmail.com>
2025-05-06 23:30:50 +0700

    comment

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router";
+// import { Outlet } from "react-router";
 import MenuButton from "../component/MenuButton";
 import { useGSAP } from "@gsap/react";
 import SplitText from "gsap/SplitText";
